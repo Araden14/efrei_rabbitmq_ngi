9da508385358520497e85af9e33ea0dba9b86cd2
Araden14 <arnaud-danselme@hotmail.com>
2025-04-29 16:42:57 +0200

    [ETAPE 1] Finalisation de la boucle de requêtes

--- src/producer.js
+++ src/producer.js
@@ -6,15 +6,13 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 const rabbitmq_url = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
-const exchange = 'AVG_project_calc_exchange';
-const message = 'Bonjour !' + Date.now();
 
 const operations = ["add", "sub", "mul", "div"];
 
 function createCalc(){
     const first = random.int(0,100);
     const second = random.int(0,1000);
-    const query = "{ 'n1': "+first+", 'n2': "+second+"}";
+    const query = { n1: first, n2: second };
 
     const operation = operations[random.int(0,3)];
 
@@ -28,18 +26,21 @@ async function send() {
     // Création du channel
     const channel = await connection.createChannel();
 
-    await channel.assertExchange(exchange, "direct", { durable: false });
+    const { queue } = await channel.assertQueue("add_worker")
 
-    const {query, operation} = createCalc();
-    channel.publish(exchange, operation, Buffer.from(query));
+    const [query, operation] = createCalc();
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(query)));
 
     console.log("Message envoyé");
 
-    // Fermeture de la connexion
-    setTimeout(function () {
-        connection.close();
-        process.exit(0);
-    }, 200);
+    return
 }
 
-send();
+// Fonction pour envoyer des messages indéfiniment toutes les 5 secondes
+function sendMessagesIndefinitely() {
+    send().catch(err => console.error('Error sending message:', err));
+    
+    setTimeout(sendMessagesIndefinitely, 5000);
+}
+sendMessagesIndefinitely();
