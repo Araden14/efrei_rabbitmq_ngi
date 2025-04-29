131949a17dc17b7b947a9debee7ea4f0adb0a5d4
Araden14 <arnaud-danselme@hotmail.com>
2025-04-29 17:03:59 +0200

    Add RabbitMQ workers for division, multiplication, and subtraction operations with message handling and result queue integration

---
+++ src/workers/workerdiv.js
@@ -0,0 +1,47 @@
+import amqplib from 'amqplib';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+
+const rabbitmq_url = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
+const queue = 'div_worker';
+const results_queue = "results"
+
+
+const connection = await amqplib.connect(rabbitmq_url);
+
+async function receive_div() {
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue, { durable: true, autoDelete: false });
+    await channel.assertQueue(results_queue, { durable: true, autoDelete: false})
+
+    channel.consume(queue, (msg) => {
+        if (msg !== null) {
+            console.log("Message reçu : ", msg.content.toString());
+            const content = JSON.parse(msg.content.toString().replace(/'/g, '"'));
+            console.log(`Requête de division reçue : ${content.n1} / ${content.n2}`);
+            
+            // Simuler un traitement long
+            const waitTime = Math.floor(Math.random() * 10000) + 5000;
+
+            if (content.n2 === 0) {
+                console.log("Division par zéro impossible");
+                channel.ack(msg);
+                return;
+            }
+            setTimeout(() => {
+                const result = { 
+                    n1: content.n1,
+                    n2: content.n2,
+                    op: "div",
+                    result: content.n1 / content.n2 
+                };
+                console.log(result);
+                channel.sendToQueue(results_queue, Buffer.from(JSON.stringify(result)));
+                channel.ack(msg);
+            }, waitTime);
+        }
+    });
+}
+
+receive_div();
---
+++ src/workers/workermul.js
@@ -0,0 +1,45 @@
+import amqplib from 'amqplib';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+
+const rabbitmq_url = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
+const queue = 'mul_worker';
+const results_queue = "results"
+const exchange = "operation"
+const routing_key = "mul"
+
+const connection = await amqplib.connect(rabbitmq_url);
+
+async function receive_div() {
+    const channel = await connection.createChannel();
+    await channel.assertExchange(exchange, "direct", { durable: true, autoDelete: false });
+    await channel.assertQueue(queue, { durable: true, autoDelete: false})
+    await channel.assertQueue(results_queue, { durable: true, autoDelete: false})
+    await channel.bindQueue(queue, exchange, routing_key);
+
+    channel.consume(queue, (msg) => {
+        if (msg !== null) {
+            console.log("Message reçu : ", msg.content.toString());
+            const content = JSON.parse(msg.content.toString().replace(/'/g, '"'));
+            console.log(`Requête de multiplication reçue : ${content.n1} * ${content.n2}`);
+            
+            // Simuler un traitement long
+            const waitTime = Math.floor(Math.random() * 10000) + 5000;
+            
+            setTimeout(() => {
+                const result = { 
+                    n1: content.n1,
+                    n2: content.n2,
+                    op: "mul",
+                    result: content.n1 * content.n2 
+                };
+                console.log(result);
+                channel.sendToQueue(results_queue, Buffer.from(JSON.stringify(result)));
+                channel.ack(msg);
+            }, waitTime);
+        }
+    });
+}
+
+receive_div();
