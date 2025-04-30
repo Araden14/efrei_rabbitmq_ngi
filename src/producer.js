08c761a20efc7ce8f20aa2069f2d6b8aecf989a3
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2025-04-30 10:10:57 +0200

    fanout s/ test | pb .env

--- src/producer.js
+++ src/producer.js
@@ -1,47 +1,66 @@
+import random from "random";
+import amqplib from "amqplib";
+import dotenv from "dotenv";
 
-import random from 'random';
-import amqplib from 'amqplib';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: "../.env" });
 
 const rabbitmq_url = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
 const exchange = "AVG_operations";
+const exchangeAll = "AVG_operations_all";
 
-const operations = ["add", "sub", "mul", "div"];
+const operations = ["add", "sub", "mul", "div", "all"];
 
-function createCalc(){
-    const first = random.int(0,100);
-    const second = random.int(0,1000);
+function createCalc(operations) {
+    const first = random.int(0, 100);
+    const second = random.int(0, 1000);
     const query = { n1: first, n2: second };
 
-    const operation = operations[random.int(0,3)];
+    const operation = operations[random.int(0, 4)];
 
     return [query, operation];
 }
 
-async function send() {
+async function send(exchange, exchangeAll, operations) {
     // Connexion
     const connection = await amqplib.connect(rabbitmq_url);
 
     // Création du channel
     const channel = await connection.createChannel();
 
-    await channel.assertExchange(exchange, "direct", { durable: true });
+    const [query, operation] = createCalc(operations);
 
-    const [query, operation] = createCalc();
+    if (operation != "all") {
+        await channel.assertExchange(exchange, "direct", {
+            durable: true,
+            autoDelete: false,
+        });
 
-    channel.publish(exchange, operation, Buffer.from(JSON.stringify(query)));
+        channel.publish(
+            exchange,
+            operation,
+            Buffer.from(JSON.stringify(query))
+        );
 
-    console.log("Message envoyé avec l'opération : ", operation);
+        console.log("Message envoyé avec l'opération : ", operation);
 
-    return
+        return;
+    }
+
+    await channel.assertExchange(exchangeAll, "fanout", { durable: true });
+
+    channel.publish(exchange, "", Buffer.from(JSON.stringify(query)));
+
+    console.log("Message envoyé avec l'opération : all");
+
+    return;
 }
 
 // Fonction pour envoyer des messages indéfiniment toutes les 5 secondes
-function sendMessagesIndefinitely() {
-    send().catch(err => console.error('Error sending message:', err));
-    
+function sendMessagesIndefinitely(exchange, exchangeAll, operations) {
+    send(exchange, exchangeAll, operations).catch((err) =>
+        console.error("Error sending message:", err)
+    );
+
     setTimeout(sendMessagesIndefinitely, 3000);
 }
-sendMessagesIndefinitely();
+sendMessagesIndefinitely(exchange, exchangeAll, operations);
--- src/worker.js
+++ src/worker.js
@@ -1,37 +1,48 @@
-import amqplib from 'amqplib';
-import dotenv from 'dotenv';
+import amqplib from "amqplib";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: "../.env" });
 
 const rabbitmq_url = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
-console.log(rabbitmq_url)
-const queue = `${process.argv[2]}_worker`
-const exchange = "AVG_operations"
-const routing_key = process.argv[2]
-const results_queue = "results"
+console.log(rabbitmq_url);
+const queue = `${process.argv[2]}_worker`;
+const exchange = "AVG_operations";
+const exchangeAll = "AVG_operations_all";
+const routing_key = process.argv[2];
+const results_queue = "results";
 const operation = {
-    "add": "+",
-    "sub": "-",
-    "mul": "*",
-    "div": "/"
-}
-
+    add: "+",
+    sub: "-",
+    mul: "*",
+    div: "/",
+};
 
 const connection = await amqplib.connect(rabbitmq_url);
 
-async function receive_add() {
+async function receive() {
     const channel = await connection.createChannel();
-    await channel.assertExchange(exchange, "direct", { durable: true, autoDelete: false });
-    await channel.assertQueue(queue, { durable: true, autoDelete: false})
-    await channel.assertQueue(results_queue, { durable: true, autoDelete: false})
+    await channel.assertExchange(exchange, "direct", {
+        durable: true,
+        autoDelete: false,
+    });
+    await channel.assertQueue(queue, { durable: true, autoDelete: false });
+    await channel.assertQueue(results_queue, {
+        durable: true,
+        autoDelete: false,
+    });
     await channel.bindQueue(queue, exchange, routing_key);
+    await channel.bindQueue(queue, exchangeAll, "");
 
     channel.consume(queue, (msg) => {
         if (msg !== null) {
             console.log("Message reçu : ", msg.content.toString());
-            const content = JSON.parse(msg.content.toString().replace(/'/g, '"'));
-            console.log(`Requête ${operation[routing_key]} reçue : ${content.n1} ${operation[routing_key]} ${content.n2}`);
-            
+            const content = JSON.parse(
+                msg.content.toString().replace(/'/g, '"')
+            );
+            console.log(
+                `Requête ${operation[routing_key]} reçue : ${content.n1} ${operation[routing_key]} ${content.n2}`
+            );
+
             if (routing_key === "div" && content.n2 === 0) {
                 console.log("Division par zéro impossible");
                 channel.ack(msg);
@@ -39,20 +50,23 @@ async function receive_add() {
             }
             // Simuler un traitement long
             const waitTime = Math.floor(Math.random() * 10000) + 5000;
-            
+
             setTimeout(() => {
-                const result = { 
+                const result = {
                     n1: content.n1,
                     n2: content.n2,
                     op: routing_key,
-                    result: content.n1 + content.n2 
+                    result: content.n1 + content.n2,
                 };
                 console.log(result);
-                channel.sendToQueue(results_queue, Buffer.from(JSON.stringify(result)));
+                channel.sendToQueue(
+                    results_queue,
+                    Buffer.from(JSON.stringify(result))
+                );
                 channel.ack(msg);
             }, waitTime);
         }
     });
 }
 
-receive_add();
+receive();
